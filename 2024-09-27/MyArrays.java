a8ae22211be45598d84a78c676b9b3c3825cb66f
Raphael Ramot <raphaelr20@nycstudents.net>
2024-09-29 10:54:18 -0400

    aryToString-tests

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -9,4 +9,11 @@ public class MyArrays {
     str += "]";
     return str;
   }
+  public static void main(String[] args) {
+    System.out.println("aryToString TEST CASES");
+    System.out.println("Expected: \"[0,1,2,3]\", actual: " + aryToString(new int[]{0,1,2,3}));
+    System.out.println("Expected: \"[]\", actual: " + aryToString(new int[0]));
+    System.out.println("Expected: \"[463]\", actual: " + aryToString(new int[]{463}));
+    System.out.println("Expected: \"[-8,20,-103,4,6983]\", actual: " + aryToString(new int[]{-8,20,-103,4,6983}));
+  }
 }
