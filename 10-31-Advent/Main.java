a024f5344ed17dbecbae7d3c8770c34aa7ba4eb4
Raphael Ramot <raphaelr20@nycstudents.net>
2024-11-04 18:58:41 -0500

    testing countTriangles

--- 10-31-Advent/Main.java
+++ 10-31-Advent/Main.java
@@ -15,15 +15,18 @@ class Main {
       //   System.out.println(input.nextLine());
       // }
 
-      Scanner sc1 = new Scanner("This is a bunch of words");
-      while(sc1.hasNext()){
-        System.out.println(sc1.next());
-      }
-      Scanner sc2 = new Scanner("10.0 1.04 99.0 -3.0 4.2");
-      double sum = 0.0;
-      while(sc2.hasNextDouble()){
-        sum+=sc2.nextDouble();//read all the numbers and add to sum
-      }
-      System.out.println(sum);
+      // Scanner sc1 = new Scanner("This is a bunch of words");
+      // while(sc1.hasNext()){
+      //   System.out.println(sc1.next());
+      // }
+      // Scanner sc2 = new Scanner("10.0 1.04 99.0 -3.0 4.2");
+      // double sum = 0.0;
+      // while(sc2.hasNextDouble()){
+      //   sum+=sc2.nextDouble();//read all the numbers and add to sum
+      // }
+      // System.out.println(sum);
+
+      // System.out.println(TriangleTester.countTrianglesA("inputTri.txt"));
+      System.out.println(TriangleTester.countTrianglesB("inputTri.txt"));
     }
   }
