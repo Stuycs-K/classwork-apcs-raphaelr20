0556e124d69b42292989fb0245ab5fc928df1ffb
Raphael Ramot <raphaelr20@nycstudents.net>
2024-11-07 14:27:42 -0500

    fixing pull issues

--- 10-31-Advent/Main.java
+++ 10-31-Advent/Main.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
-      String text = "This is a sample\nOf how to read\ntext!";
+      // String text = "This is a sample\nOf how to read\ntext!";
 
       // creates a Scanner object to read throug the String
-      Scanner input = new Scanner(text);
+      // Scanner input = new Scanner(text);
 
       //you don't want a for loop here unless you know the size of the data set
 
@@ -27,6 +27,6 @@ class Main {
       // System.out.println(sum);
 
       // System.out.println(TriangleTester.countTrianglesA("inputTri.txt"));
-      System.out.println(TriangleTester.countTrianglesB("inputTri.txt"));
+      // System.out.println(TriangleTester.countTrianglesB("inputTri.txt"));
     }
   }
