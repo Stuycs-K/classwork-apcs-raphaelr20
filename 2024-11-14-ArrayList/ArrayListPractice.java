5511630687705149f1891732f83f67e6f8702056
Raphael Ramot <raphaelr20@nycstudents.net>
2024-11-16 17:37:16 -0500

    makeReversedList

--- 2024-11-14-ArrayList/ArrayListPractice.java
+++ 2024-11-14-ArrayList/ArrayListPractice.java
@@ -12,7 +12,7 @@ public class ArrayListPractice {
         }
         return arr;
     }
-    public static void replaceEmpty( ArrayList<String> original){
+    public static void replaceEmpty(ArrayList<String> original){
         //Modify the ArrayList such that it has all of the empty strings are
         //replaced with the word "Empty".
         for (int i = 0; i<original.size(); i++) {
@@ -20,4 +20,13 @@ public class ArrayListPractice {
                 original.set(i, "Empty");
         }
     }
+    public static ArrayList<String> makeReversedList(ArrayList<String> original){
+        //return a new ArrayList that is in the reversed order of the original.
+        ArrayList<String> reverse = new ArrayList<String>(original.size());
+        for (int i = original.size()-1; i>=0; i--) {
+            reverse.add(original.get(i));
+        }
+        return reverse;
+    }
+    
 }
