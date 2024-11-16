e9de46212aceaeacd0d1ee6cbaf4a3b558c09e02
Raphael Ramot <raphaelr20@nycstudents.net>
2024-11-16 17:30:00 -0500

    createRandomArray

---
+++ 2024-11-14-ArrayList/ArrayListPractice.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class ArrayListPractice {
+    public static ArrayList<String> createRandomArray(int size) {
+        ArrayList<String> arr = new ArrayList<String>(size);
+        for (int i = 0; i<size; i++) {
+            int value = Math.random()*10;
+            if (value == 0)
+                arr.add("");
+            else
+                arr.add(value+"");
+        }
+        return arr;
+    }
+    public static void replaceEmpty( ArrayList<String> original){
+        //Modify the ArrayList such that it has all of the empty strings are
+        //replaced with the word "Empty".
+    }
+}
