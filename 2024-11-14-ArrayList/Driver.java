0b88d7d6280c292c55e645db0effcbb8ee3188b7
Raphael Ramot <raphaelr20@nycstudents.net>
2024-11-16 18:17:51 -0500

    testing

---
+++ 2024-11-14-ArrayList/Driver.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+
+public class Driver {
+    public static void main(String[] args) {
+        // ArrayList<String> arr1 = ArrayListPractice.createRandomArray(10);
+        // System.out.println(arr1);
+        // ArrayListPractice.replaceEmpty(arr1);
+        // System.out.println(arr1);
+        // System.out.println(ArrayListPractice.makeReversedList(arr1));
+        // ArrayList<String> arr2 = ArrayListPractice.createRandomArray(0);
+        // System.out.println(arr2);
+        // ArrayListPractice.replaceEmpty(arr2);
+        // System.out.println(arr2);
+        // System.out.println(ArrayListPractice.makeReversedList(arr2));
+        // ArrayList<String> arr3 = ArrayListPractice.createRandomArray(1);
+        // System.out.println(arr3);
+        // ArrayListPractice.replaceEmpty(arr3);
+        // System.out.println(arr3);
+        // System.out.println(ArrayListPractice.makeReversedList(arr3));
+        // ArrayList<String> arr4 = ArrayListPractice.createRandomArray(200000);
+        // System.out.println(arr4);
+        // ArrayListPractice.replaceEmpty(arr4);
+        // System.out.println(arr4);
+        // ArrayList<String> revArr4 = ArrayListPractice.makeReversedList(arr4);
+        // ArrayList<String> a = ArrayListPractice.createRandomArray(4);
+        // ArrayList<String> b = ArrayListPractice.createRandomArray(8);
+        // System.out.println(a+", "+b);
+        // System.out.println(ArrayListPractice.mixLists(a, b));
+        // ArrayList<String> a = ArrayListPractice.createRandomArray(82);
+        // ArrayList<String> b = ArrayListPractice.createRandomArray(3);
+        // System.out.println(a+", "+b);
+        // ArrayList<String> mixab = ArrayListPractice.mixLists(a, b);
+        // System.out.println(mixab);
+        // ArrayListPractice.replaceEmpty(mixab);
+        // System.out.println(mixab);
+        // ArrayListPractice.mixLists(ArrayListPractice.createRandomArray(300000), ArrayListPractice.createRandomArray(80000));
+        // runtime did not slow down with any size lists
+    }
+}
