b0477220f06ca44525b399d576b0d2fa3e2f9838
Raphael Ramot <raphaelr20@nycstudents.net>
2024-11-16 17:33:19 -0500

    replaceEmpty

--- 2024-11-14-ArrayList/ArrayListPractice.java
+++ 2024-11-14-ArrayList/ArrayListPractice.java
@@ -15,5 +15,9 @@ public class ArrayListPractice {
     public static void replaceEmpty( ArrayList<String> original){
         //Modify the ArrayList such that it has all of the empty strings are
         //replaced with the word "Empty".
+        for (int i = 0; i<original.size(); i++) {
+            if (original.get(i).equals(""))
+                original.set(i, "Empty");
+        }
     }
 }
