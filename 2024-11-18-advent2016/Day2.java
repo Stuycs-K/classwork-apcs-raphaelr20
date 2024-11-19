74f11baa67b1e92cae6edc78bfe1df82e3169123
Raphael Ramot <raphaelr20@nycstudents.net>
2024-11-19 10:15:07 -0500

    passkey

--- 2024-11-18-advent2016/Day2.java
+++ 2024-11-18-advent2016/Day2.java
@@ -15,7 +15,26 @@ public class Day2 {
       return null;
     }
   }
+  public static String passkey(String[][] moves) {
+    String code = "";
+    int place = 5;
+    for (int i = 0; i<moves.length; i++) {
+      for (int x = 0; x<moves[i].length; x++) {
+        if (moves[i][x].equals("U") && place>3)
+          place-=3;
+        else if (moves[i][x].equals("D") && place<7)
+          place+=3;
+        else if (moves[i][x].equals("L") && place!=1 && place!=4 && place!=7)
+          place-=1;
+        else if (place!=3 && place!=6 && place!=9)
+          place+=1;
+      }
+      code+=place;
+    }
+    return code;
+  }
   public static void main(String[] args) {
     System.out.println(Arrays.deepToString(parse("Day2.txt")));
+    System.out.println(passkey(parse("Day2.txt")));
   }
 }
