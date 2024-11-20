6f2dfdec851010032ac01ee38a24f20e63e28269
Raphael Ramot <raphaelr20@nycstudents.net>
2024-11-19 20:29:44 -0500

    passkey2

--- 2024-11-18-advent2016/Day2.java
+++ 2024-11-18-advent2016/Day2.java
@@ -33,8 +33,37 @@ public class Day2 {
     }
     return code;
   }
+  public static String passkey2(String[][] moves) {
+    String code = "";
+    int place = 5;
+    for (int i = 0; i<moves.length; i++) {
+      for (int x = 0; x<moves[i].length; x++) {
+        if (moves[i][x].equals("U") && place!=1 && place!=2 && place!=4 && place!=5 && place!=9)
+          place-=4;
+        else if (moves[i][x].equals("D") && place!=5 && place!=9 && place!=10 && place!=12 && place!=13)
+          place+=4;
+        else if (moves[i][x].equals("L") && place!=1 && place!=2 && place!=5 && place!=10 && place!=13)
+          place-=1;
+        else if (moves[i][x].equals("R") && place!=1 && place!=4 && place!=9 && place!=12 && place!=13)
+          place+=1;
+      }
+      System.out.println(place);
+      if (place==10)
+        code+="A";
+      else if (place==11)
+        code+="B";
+      else if (place==12)
+        code+="C";
+      else if (place==13)
+        code+="D";
+      else
+        code+=place;
+    }
+    return code;
+  }
   public static void main(String[] args) {
     System.out.println(Arrays.deepToString(parse("Day2.txt")));
-    System.out.println(passkey(parse("Day2.txt")));
+    // System.out.println(passkey(parse("Day2.txt")));
+    System.out.println(passkey2(parse("Day2.txt")));
   }
 }
