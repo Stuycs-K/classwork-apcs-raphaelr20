1aae6b2f3f8049093b4fff61bb6d189268602cf0
Raphael Ramot <raphaelr20@nycstudents.net>
2024-11-19 20:10:20 -0500

    deleting a file

--- 2024-11-18-advent2016/Driver.java
+++
@@ -1,9 +0,0 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
-
-public class Driver {
-    public static void main(String[] args) {
-        System.out.println(Day1.findDistance("Day1.txt"));
-    }
-}
