ec8019ce98813c9f3ca6d04a6829e4aea0d834fa
Raphael Ramot <raphaelr20@nycstudents.net>
2024-11-21 03:38:30 -0500

    part 2

--- 2024-11-18-advent2016/Day1.java
+++ 2024-11-18-advent2016/Day1.java
@@ -1,50 +1,90 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Day1 {
-  public static int findDistance(String filename) {
+  public static String[] parse(String filename) {
     try {
       File file=new File (filename);
       Scanner input = new Scanner(file);
-      int x = 0;
-      int y = 0;
-      int direction = 0;
       String[] turns = input.nextLine().split(", ");
-      System.out.println(Arrays.toString(turns));
-      for (int i = 0; i<turns.length; i++) {
-        if (turns[i].substring(0,1).equals("L")) {
-          direction -= 90;
-        }
-        else {
-          direction += 90;
-        }
-        direction = direction%360;
-        if (direction < 0) {
-          direction += 360;
-        }
-        int distance = Integer.parseInt(turns[i].substring(1));
-        if (direction == 0) {
-          y += distance;
-        }
-        else if (direction == 90) {
-          x += distance;
-        }
-        else if (direction == 180) {
-          y -= distance;
-        }
-        else if (direction == 270) {
-          x -= distance;
-        }
-      }
-      return Math.abs(x)+Math.abs(y);
+      input.close();
+      return turns;
     } catch (FileNotFoundException ex) {
-        System.out.println("File not found");
-        return -1;
+      System.out.println("File not found");
+      return null;
+  }
+  }
+  public static int findDistance(String[] turns) {
+    int x = 0;
+    int y = 0;
+    int direction = 0;
+    for (int i = 0; i<turns.length; i++) {
+      if (turns[i].substring(0,1).equals("L")) {
+        direction -= 90;
+      }
+      else {
+        direction += 90;
+      }
+      direction = direction%360;
+      if (direction < 0) {
+        direction += 360;
+      }
+      int distance = Integer.parseInt(turns[i].substring(1));
+      if (direction == 0) {
+        y += distance;
+      }
+      else if (direction == 90) {
+        x += distance;
+      }
+      else if (direction == 180) {
+        y -= distance;
+      }
+      else if (direction == 270) {
+        x -= distance;
+      }
     }
+    return Math.abs(x)+Math.abs(y);
+  }
+
+  public static int findDistance2(String[] turns) {
+    int x = 0;
+    int y = 0;
+    int direction = 0;
+    ArrayList<int[]> locations = new ArrayList<int[]>();
+    for (int i = 0; i<turns.length; i++) {
+      if (turns[i].substring(0,1).equals("L")) {
+        direction -= 90;
+      }
+      else {
+        direction += 90;
+      }
+      direction = direction%360;
+      if (direction < 0) {
+        direction += 360;
+      }
+      int distance = Integer.parseInt(turns[i].substring(1));
+      if (direction == 0) {
+        y += distance;
+      }
+      else if (direction == 90) {
+        x += distance;
+      }
+      else if (direction == 180) {
+        y -= distance;
+      }
+      else if (direction == 270) {
+        x -= distance;
+      }
+      for (int c = 0; c<locations.size(); c++) {
+        if (locations.get(c)[0]==x && locations.get(c)[1]==y)
+          return Math.abs(x)+Math.abs(y);
+      }
+      locations.add(new int[]{x,y});
+    }
+    return -1;
   }
   public static void main(String[] args) {
-      System.out.println(findDistance("Day1.txt"));
+      System.out.println(findDistance(parse("Day1.txt")));
+      System.out.println(findDistance2(parse("Day1.txt")));
   }
 }
