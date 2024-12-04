eea3836f8cb9aeaf74d60f708d4a5188c24ec06a
Raphael Ramot <raphaelr20@nycstudents.net>
2024-12-03 19:36:14 -0500

    snake

--- 2024-12-05-colors/ColorDemo.java
+++ 2024-12-05-colors/ColorDemo.java
@@ -1,5 +1,12 @@
 public class ColorDemo {
     public static void main(String[] args) {
-        
+        for (int i = 0; i<10; i++) {
+            for (int x = 0; x<8; x++) {
+                System.out.print("\u001b[4"+x+"m.");
+            }
+            for (int x = 7; x>=0; x--) {
+                System.out.print("\u001b[4"+x+"m.");
+            }
+        }
     }
 }
