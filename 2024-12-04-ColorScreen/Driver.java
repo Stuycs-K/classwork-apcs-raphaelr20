629d91f0d442f56b000b506ca5b0038c76d574d1
Raphael Ramot <raphaelr20@nycstudents.net>
2024-12-04 21:52:46 -0500

    integers

--- 2024-12-04-ColorScreen/Driver.java
+++ 2024-12-04-ColorScreen/Driver.java
@@ -5,19 +5,47 @@ public class Driver {
         for (int i = 0; i<80; i++) {
             System.out.print('I');
         }
-        for (int i = 1; i<30; i++) {
+        for (int i = 1; i<29; i++) {
             Text.go(i,1);
             System.out.print('I');
             Text.go(i, 80);
             System.out.print('I');
         }
-        Text.go(31,1);
+        Text.go(30,1);
         for (int i = 0; i<80; i++) {
             System.out.print('I');
         }
     }
+    public static int[] randomArray(int size) {
+        int[]randArr = new int[size];
+        for (int i = 0; i<size; i++) {
+            randArr[i] = (int)(Math.random()*100);
+        }
+        return randArr;
+    }
+    public static void placeInts() {
+        // where the integers will go
+        int[] places = new int[]{19,40,59};
+        int[] nums = randomArray(3);
+        for (int i = 0; i<3; i++) {
+            Text.go(2,places[i]);
+            if (nums[i]<25)
+                Text.color(1,Text.RED);
+            else if (nums[i]>75)
+                Text.color(1,Text.GREEN);
+            else 
+                Text.color(Text.WHITE);
+            System.out.print(nums[i]);
+        }
+    }
     public static void main(String[] args) {
         System.out.print(Text.CLEAR_SCREEN);
+        System.out.print(Text.HIDE_CURSOR);
+        placeInts();
+        System.out.print(Text.RESET);
         createBorder();
+        System.out.print(Text.RESET);
+        System.out.println();
+        Text.go(31,1);
     }
 }
