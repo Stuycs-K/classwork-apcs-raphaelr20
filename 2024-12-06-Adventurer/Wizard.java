e9fe33100a246689619368ac2ef2745e563ac49f
Raphael Ramot <raphaelr20@nycstudents.net>
2024-12-06 14:36:54 -0500

    specialAttack and edits

--- 2024-12-06-Adventurer/Wizard.java
+++ 2024-12-06-Adventurer/Wizard.java
@@ -2,6 +2,7 @@ import java.util.Random;
 public class Wizard extends Adventurer {
     private int special;
     private int maxSpecial;
+    
     public Wizard(String name) {
         super(name);
         special = 3;
@@ -35,26 +36,35 @@ public class Wizard extends Adventurer {
             return "fire spell - "+other.getName()+" deafeated";
         }
         other.applyDamage(2);
-        return "fire spell - damaged enemy "+other.getName()+" 2 HP";
+        return "fire spell - damaged enemy "+other.getName()+" 2 HP, "+other.getName()+" has "+other.getHP()+" HP remaining";
     }
     public String support(Adventurer other) {
         if (other.getmaxHP()-other.getHP()<4) {
             other.setHP(other.getmaxHP());
-            return "heal spell - ally "+other.getName()+" at max health";
+            return "heal spell - ally "+other.getName()+" at max health ("+other.getmaxHP()+" HP)";
         }
         other.setHP(other.getHP()+3);
-        return "heal spell - healed ally "+other.getName()+" 3 HP";
+        return "heal spell - healed ally "+other.getName()+" 3 HP, "+other.getName()+" has "+other.getHP()+" HP";
     }
 
     public String support() {
         if (this.getmaxHP()-this.getHP()<6) {
             this.setHP(this.getmaxHP());
-            return "heal spell - "+this.getName()+" is at max health";
+            return "heal spell - "+this.getName()+" is at max health ("+this.getmaxHP()+" HP)";
         }
         this.setHP(this.getHP()+5);
-        return "heal spell - healed self 5 HP";
+        return "heal spell - healed self 5 HP to "+this.getHP()+" HP";
     }
     public String specialAttack(Adventurer other) {
-
+        if (this.getSpecial() == 0) {
+            return "I do not have enough "+this.getSpecialName()+" to use special attack";
+        }
+        this.setSpecial(this.getSpecial()-1);
+        if (other.getHP()<6) {
+            other.setHP(0);
+            return "special spell - "+other.getName()+" deafeated, "+this.getSpecial()+" "+this.getSpecialName()+" left";
+        }
+        other.applyDamage(5);
+        return "special spell - damaged enemy "+other.getName()+" 5 HP, "+other.getName()+" has "+other.getHP()+" HP remaining";
     }
 }
