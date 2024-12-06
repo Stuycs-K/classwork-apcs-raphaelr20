5baf6ffaa0a24138f1263562fdaea4c17f485295
Raphael Ramot <raphaelr20@nycstudents.net>
2024-12-06 09:46:06 -0500

    constructors

---
+++ 2024-12-06-Adventurer/Wizard.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+public class Wizard extends Adventurer {
+    public Wizard(String name) {
+        super(name);
+    }
+    public Wizard(String name, int health) {
+        super(name,health);
+    }
+
+    
+}
