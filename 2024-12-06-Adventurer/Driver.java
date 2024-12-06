70a6b21c9353b54213f8ee4f563a04d23ca5c5e1
Raphael Ramot <raphaelr20@nycstudents.net>
2024-12-06 14:56:02 -0500

    basic testing

---
+++ 2024-12-06-Adventurer/Driver.java
@@ -0,0 +1,30 @@
+public class Driver {
+    public static void main(String[] args) {
+        Adventurer p1 = new Wizard("John", 15, 5);
+        Adventurer p2 = new Wizard("Evil James");
+        System.out.println(p1.getSpecialName()+" should be magical mushrooms");
+        System.out.println(p2.getSpecialName()+" should be magical mushrooms");
+        System.out.println(p1.getSpecial()+" should be 5");
+        System.out.println(p2.getSpecial()+" should be 3");
+        p1.setSpecial(1);
+        System.out.println(p1.getSpecial()+" should be 1");
+        p2.setSpecial(10);
+        System.out.println(p2.getSpecial()+" should be 3");
+        p2.setSpecial(-4);
+        System.out.println(p2.getSpecial()+" should be 0");
+        System.out.println(p1.getSpecialMax()+" should be 5");
+        System.out.println(p2.getSpecialMax()+" should be 3");
+
+        System.out.println(p1.attack(p2)+", should be fire spell - damaged enemy Evil James 2 HP, Evil James has 8 HP remaining");
+        p2.setHP(1);
+        System.out.println(p1.attack(p2)+", should be fire spell - Evil James defeated");
+        p2.setHP(3);
+        System.out.println(p1.attack(p2)+", should be fire spell - damaged enemy Evil James 2 HP, Evil James has 1 HP remaining");
+        p2.setHP(-1);
+        System.out.println(p1.attack(p2)+", should be fire spell - Evil James defeated");
+        System.out.println(p2.getHP()+" should be 0");
+        System.out.println(p2.support()+ ", should be: heal spell - healed self 5 HP to 5 HP");
+        System.out.println(p1.support(p2)+", should be: heal spell - healed ally Evil James 3 HP, Evil James has 8 HP");
+        System.out.println(p2.support()+ ", should be: heal spell - Evil James is at max health (10 HP)");
+    }
+}
