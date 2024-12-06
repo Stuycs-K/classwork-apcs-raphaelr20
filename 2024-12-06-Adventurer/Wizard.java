2d1cf80e434e147244e56f586b494a37a74a9fc7
Raphael Ramot <raphaelr20@nycstudents.net>
2024-12-06 09:56:29 -0500

    special abstract methods

--- 2024-12-06-Adventurer/Wizard.java
+++ 2024-12-06-Adventurer/Wizard.java
@@ -1,11 +1,31 @@
 import java.util.Random;
 public class Wizard extends Adventurer {
+    private int special;
+    private int maxSpecial;
     public Wizard(String name) {
         super(name);
+        special = 3;
+        maxSpecial = 3;
     }
-    public Wizard(String name, int health) {
+    public Wizard(String name, int health, int special) {
         super(name,health);
+        this.special = special;
+        maxSpecial = special;
     }
 
-    
+    public String getSpecialName() {
+        return "magical mushrooms";
+    }
+    public int getSpecial() {
+        return special;
+    }
+    public void setSpecial(int n) {
+        if (n>maxSpecial) {
+            n = maxSpecial;
+        }
+        special = n;
+    }
+    public int getSpecialMax() {
+        return maxSpecial;
+    }
 }
