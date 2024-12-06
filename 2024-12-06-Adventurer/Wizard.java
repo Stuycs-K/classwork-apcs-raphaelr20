700c206acd1bff0b59f9279a7d01ac664cfce84e
Raphael Ramot <raphaelr20@nycstudents.net>
2024-12-06 10:14:56 -0500

    supports and attack

--- 2024-12-06-Adventurer/Wizard.java
+++ 2024-12-06-Adventurer/Wizard.java
@@ -28,4 +28,33 @@ public class Wizard extends Adventurer {
     public int getSpecialMax() {
         return maxSpecial;
     }
+
+    public String attack(Adventurer other) {
+        if (other.getHP()<3) {
+            other.setHP(0);
+            return "fire spell - "+other.getName()+" deafeated";
+        }
+        other.applyDamage(2);
+        return "fire spell - damaged enemy "+other.getName()+" 2 HP";
+    }
+    public String support(Adventurer other) {
+        if (other.getmaxHP()-other.getHP()<4) {
+            other.setHP(other.getmaxHP());
+            return "heal spell - ally "+other.getName()+" at max health";
+        }
+        other.setHP(other.getHP()+3);
+        return "heal spell - healed ally "+other.getName()+" 3 HP";
+    }
+
+    public String support() {
+        if (this.getmaxHP()-this.getHP()<6) {
+            this.setHP(this.getmaxHP());
+            return "heal spell - "+this.getName()+" is at max health";
+        }
+        this.setHP(this.getHP()+5);
+        return "heal spell - healed self 5 HP";
+    }
+    public String specialAttack(Adventurer other) {
+
+    }
 }
