09655fe55bad5d8b3213090a6b405604dbfacfe3
Raphael Ramot <raphaelr20@nycstudents.net>
2024-12-08 19:10:29 -0500

    more tests

--- 2024-12-06-Adventurer/Driver.java
+++ 2024-12-06-Adventurer/Driver.java
@@ -15,16 +15,21 @@ public class Driver {
         System.out.println(p1.getSpecialMax()+" should be 5");
         System.out.println(p2.getSpecialMax()+" should be 3");
 
-        System.out.println(p1.attack(p2)+", should be fire spell - damaged enemy Evil James 2 HP, Evil James has 8 HP remaining");
+        System.out.println(p1.attack(p2)+", should be: fire spell - damaged enemy Evil James 2 HP, Evil James has 8 HP remaining");
         p2.setHP(1);
-        System.out.println(p1.attack(p2)+", should be fire spell - Evil James defeated");
+        System.out.println(p1.attack(p2)+", should be: fire spell - Evil James defeated");
         p2.setHP(3);
-        System.out.println(p1.attack(p2)+", should be fire spell - damaged enemy Evil James 2 HP, Evil James has 1 HP remaining");
+        System.out.println(p1.attack(p2)+", should be: fire spell - damaged enemy Evil James 2 HP, Evil James has 1 HP remaining");
         p2.setHP(-1);
-        System.out.println(p1.attack(p2)+", should be fire spell - Evil James defeated");
+        System.out.println(p1.attack(p2)+", should be: fire spell - Evil James defeated");
         System.out.println(p2.getHP()+" should be 0");
         System.out.println(p2.support()+ ", should be: heal spell - healed self 5 HP to 5 HP");
         System.out.println(p1.support(p2)+", should be: heal spell - healed ally Evil James 3 HP, Evil James has 8 HP");
         System.out.println(p2.support()+ ", should be: heal spell - Evil James is at max health (10 HP)");
+        System.out.println(p2.specialAttack(p1)+", should be: I do not have enough magical mushrooms to use special attack");
+        System.out.println(p2.restoreSpecial(6)+" should be 3");
+        System.out.println(p2.specialAttack(p1)+", should be: special spell - damaged enemy John 5 HP, John has 10 HP remaining");
+        p2.specialAttack(p1);
+        System.out.println(p2.specialAttack(p1)+", should be: special spell - John defeated, 0 magical mushrooms left");
     }
 }
