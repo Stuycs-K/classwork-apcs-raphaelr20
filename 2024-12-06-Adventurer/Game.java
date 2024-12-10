ca17557623f7a41baa3224af51facc4c22274c3d
Raphael Ramot <raphaelr20@nycstudents.net>
2024-12-09 21:07:03 -0500

    setup edits

--- 2024-12-06-Adventurer/Game.java
+++ 2024-12-06-Adventurer/Game.java
@@ -1,29 +1,34 @@
 import java.util.Scanner;
 public class Game {
-    public static String checkCharacter() {
-        Scanner userInput = new Scanner(System.in);
-        String character = userInput.nextLine();
-        if (character.equals("K") || character.equals("G")) {
-            userInput.close();
-            return character;
-        }
-        else {
-            System.out.println("Please type G or K");
-            checkCharacter();
-        }
-    }
     public static void main(String[] args) {
-        Wizard Gandalf = new Wizard("Gandalf", 15, 3);
-        CodeWarrior K = new CodeWarrior("Mr. K");
+        Scanner userInput = new Scanner(System.in);
         System.out.println("Choose your character : Gandalf the Wizard (G) or Mr. K the CodeWarrior (K)");
-        if (checkCharacter().equals("G")) {
+        boolean flag = true;
+        String character = "";
+        while (flag) {
+            String input = userInput.nextLine();
+            if (input.equals("G") || input.equals("K")) {
+                character = input;
+                flag = false;
+            }
+            else {
+                System.out.println("Please type G or K");
+            }
+        }
+        Adventurer player;
+        Adventurer computer;
+        if (character.equals("G")) {
             System.out.println("You have selected Gandalf the Wizard.");
+            player = new Wizard("Gandalf", 15, 3);
+            computer = new CodeWarrior("Mr. K");
         }
         else {
             System.out.println("You have selected Mr. K the CodeWarrior.");
+            computer = new Wizard("Gandalf", 15, 3);
+            player = new CodeWarrior("Mr. K");
         }
-        Scanner userInput = new Scanner(System.in);
-        while (Gandalf.getHP()>0 && K.getHP()>0) {
+        
+        while (player.getHP()>0 && computer.getHP()>0) {
             
         }
         userInput.close();
